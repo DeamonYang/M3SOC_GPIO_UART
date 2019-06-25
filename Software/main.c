727443f41e7bb5994b0517733ce9e43bf1907582
GONG1455 <22903193+GONG1455@users.noreply.github.com>
2019-06-26 02:16:19 +0800

    添加LED灯的显示形式
    添加LED灯的显示形式，使展示效果更加直观明显。

--- Software/main.c
+++ Software/main.c
@@ -49,7 +49,9 @@ void send_str(char string[])
 
 int main(void) 
  {
-	unsigned int	key_value;
+	unsigned int	key_value	;
+	unsigned int	delay_time	=	2500000; 
+	unsigned int	led_value		=	0x05; 
 	AHB_GPIO_LED	=	0xf;
 	delay(5000000);	 
 	AHB_GPIO_LED	=	0x0;
@@ -61,11 +63,49 @@ int main(void)
 	 
 	while(1)
 	{
-		AHB_GPIO_LED 	= 	AHB_GPIO_KEY;		
-		send_str("hello,world!   \n");
-		
-		delay(50000);			
-			
+		key_value			=		AHB_GPIO_KEY;
+																					
+	if(	key_value ==	0x0d)		//the key value is 1101,	The key2 is pressed 
+	{
+		AHB_GPIO_LED	=	0x0e;
+		delay(delay_time);		
+		AHB_GPIO_LED	=	0x0d;;
+		delay(delay_time);
+		AHB_GPIO_LED	=	0x0b;
+		delay(delay_time);		
+		AHB_GPIO_LED	=	0x07;;
+		delay(delay_time);	
+	}	
+	else if(	key_value ==	0x0b)		//the key value is 1011,	The key3 is pressed 
+	{				
+		AHB_GPIO_LED	=	0x07;;
+		delay(delay_time);
+		AHB_GPIO_LED	=	0x0b;
+		delay(delay_time);
+		AHB_GPIO_LED	=	0x0d;;
+		delay(delay_time);
+		AHB_GPIO_LED	=	0x0e;
+		delay(delay_time);
+	}	
+	else if(	key_value ==	0x07)		//the key value is 0111,	The key4 is pressed 
+	{
+		AHB_GPIO_LED	=	0x5;
+		delay(delay_time);	 
+		AHB_GPIO_LED	=	0xa;
+		delay(delay_time);		
+	}
+	
+	else
+	{
+		AHB_GPIO_LED	=	0xf;
+		delay(delay_time);	 
+		AHB_GPIO_LED	=	0x0;
+		delay(delay_time);		
+	
+	}
+
+//		AHB_GPIO_LED 	= 	AHB_GPIO_KEY;		
+//		send_str("hello,world!   \n");		
 	}		
 	return 0;
 }
---
+++ output_files/ARM_M3_SOC.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10CL025YU256) Path("D:/01_RAM_CUP/05_M3SOC_GPIO/output_files/") File("ARM_M3_SOC.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
